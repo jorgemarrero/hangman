d760880831f981c839ed5a3f7e3abeb751386d45
Jorge <jorgems93@gmail.com>
2017-04-26 20:15:47 +0100

    Diferentes usuarios

--- script.js
+++ script.js
@@ -1,179 +1,160 @@
+var $nickname;
+var timer;
+var $finding = $("<p>", {
+    "class": "finding"
+});
+
 $(document).ready(function () {
     var paises = ["EGIPTO", "ESPAÑA", "FRANCIA", "ESTONIA", "LITUANIA", "MARRUECOS", "KENIA", "RUSIA", "CHINA", "LA INDIA", "ARGENTINA", "VENEZUELA", "CHILE", "CUBA", "QATAR", "ESTADOS UNIDOS"];
 
-    var status = {
-        countries: [
-            {
-                name: "EGIPTO",
-                clue: "Pirámides"
-        },
-            {
-                name: "ESPAÑA",
-                clue: "Toros"
-        },
-            {
-                name: "FRANCIA",
-                clue: "Champagne"
-        },
-            {
-                name: "ESTONIA",
-                clue: "Tallin"
-        },
-            {
-                name: "LITUANIA",
-                clue: "Vilna"
-        },
-            {
-                name: "MARRUECOS",
-                clue: "Té verde"
-        },
-            {
-                name: "KENYA",
-                clue: "Safari"
-        },
-            {
-                name: "RUSIA",
-                clue: "Vodka"
-        },
-            {
-                name: "CHINA",
-                clue: "Arroz tres delicias"
-        },
-            {
-                name: "LA INDIA",
-                clue: "Especias"
-        },
-            {
-                name: "ARGENTINA",
-                clue: "Asado"
-        },
-            {
-                name: "VENEZUELA",
-                clue: "Maduro"
-        },
-            {
-                name: "CHILE",
-                clue: "Santiago"
-        },
-            {
-                name: "CUBA",
-                clue: "Fidel"
-        },
-            {
-                name: "QATAR",
-                clue: "Jeque"
-        },
-            {
-                name: "ESTADOS UNIDOS",
-                clue: "Hamgurguesas"
-        }
-    ]
-    }
-
-
-
-    /* PARTIDA ACTUAL */
+    /* PARTIDA NUEVA */
     var partidaActual = {
-        numeroVidas: 10,
-        tiempo: 100,
-        toFind: paises[Math.floor(Math.random() * paises.length)],
-        finding: [],
-        pista: true,
-        letrasUsadas: []
+        //        numeroVidas: 10,
+        //        tiempo: 100,
+        //        toFind: paises[Math.floor(Math.random() * paises.length)],
+        //        finding: [],
+        //        pista: true,
+        //        letrasUsadas: [],
+        //        ganadas: 0,
+        //        perdidas: 0,
+        actual: "none",
+        "Jorge M": {
+            ganadas: 15,
+            perdidas: 20,
+            toFind: "MADRID"
+        }
     }
 
+    /**/
+
+    /* A JUGAR */
+    $("#a-jugar").on("click", function () {
+        $nickname = $("#nickname").val();
+        partidaActual.actual = $nickname;
+
+        console.log(partidaActual);
+
+        $(".pide-usuario").remove();
+
+        if (partidaActual[$nickname] != undefined) {
+            console.log("Jugador conocido")
+        } else {
+            console.log("Nuevo jugador");
+            partidaActual[$nickname] = {};
+            partidaActual[$nickname].numeroVidas = 10;
+            partidaActual[$nickname].tiempo = 100;
+            partidaActual[$nickname].toFind = paises[Math.floor(Math.random() * paises.length)];
+            partidaActual[$nickname].finding = [];
+            partidaActual[$nickname].pista = true;
+            partidaActual[$nickname].letrasUsadas = [];
+            partidaActual[$nickname].ganadas = 0;
+            partidaActual[$nickname].perdidas = 0;
+            console.log(partidaActual);
+        }
+
+        juego();
+    });
+
+    /* CERRAR SESIÓN */
+    $("#close-sesion").on("click", function () {
+        partidaActual.actual = "none";
+        localStorage.setItem("partida", JSON.stringify(partidaActual));
+        location.reload();
+    });
+
+    /* REINICIAR */
+    $("#reiniciar").on("click", function () {
+        if (!($finding[0].innerText == partidaActual[$nickname].toFind)) {
+            ++partidaActual[$nickname].perdidas;
+        }
+        partidaActual[$nickname].numeroVidas = 10;
+        partidaActual[$nickname].tiempo = 100;
+        partidaActual[$nickname].toFind = paises[Math.floor(Math.random() * paises.length)];
+        partidaActual[$nickname].finding = [];
+        partidaActual[$nickname].pista = true;
+        partidaActual[$nickname].letrasUsadas = [];
+        localStorage.setItem("partida", JSON.stringify(partidaActual));
+        location.reload();
+    });
+
+    /* COMPRUEBA SI EXISTE JUGANDO */
     if (localStorage.getItem("partida") != null) {
         partidaActual = JSON.parse(localStorage.getItem("partida"));
         console.log(partidaActual);
-    }
-    /**/
 
-    console.log(partidaActual.toFind);
-    $("#vidas").text(partidaActual.numeroVidas);
-    var $finding = $("<p>", {
-        "class": "finding"
-    });
-
-    /* HISTORICO */
-    var historico = {
-        ganadas: 0,
-        perdidas: 0
-    }
-    if (localStorage.getItem("historico") != null) {
-        historico = JSON.parse(localStorage.getItem("historico"));
-        $("#historico-ganadas").text(historico.ganadas);
-        $("#historico-perdidas").text(historico.perdidas);
-    }
-    /**/
-
-
-
-    /* ELEMENTO A BUSCAR */
-    $(".centrado").append($finding);
-
-    for (var i = 0; i < partidaActual.toFind.length; i++) {
-        if (partidaActual.finding.length > 0 + i) {
-
-        } else {
-            if (partidaActual.toFind[i] == " ") {
-                partidaActual.finding[i] = " ";
-            } else {
-                partidaActual.finding[i] = "-";
-            }
+        if (partidaActual.actual != "none") {
+            $(".pide-usuario").remove();
+            $nickname = partidaActual.actual;
+            juego();
         }
     }
-
-    $finding.text(partidaActual.finding.join(""));
-    console.log(partidaActual);
     /**/
 
-    /* TIEMPO RESTANTE */
-    var timer;
-    countDown();
+    function juego() {
+        countDown();
+        $("#vidas").text(partidaActual[$nickname].numeroVidas);
+        $("#historico-ganadas").text(partidaActual[$nickname].ganadas);
+        $("#historico-perdidas").text(partidaActual[$nickname].perdidas);
+        $("#jugador").text($nickname);
 
-    function countDown() {
-        $tiempoRestante = $("#tiempo-restante");
-        $tiempoRestante.text(partidaActual.tiempo);
+        /* ELEMENTO A BUSCAR */
+        $(".centrado").append($finding);
 
-        timer = setInterval(function () {
-            if (partidaActual.tiempo == 0) {
-                sumarPerdida();
-                clearInterval(timer);
+        for (var i = 0; i < partidaActual[$nickname].toFind.length; i++) {
+            if (partidaActual[$nickname].finding.length > 0 + i) {
+
             } else {
-                partidaActual.tiempo = partidaActual.tiempo - 1;
-                $tiempoRestante.text(partidaActual.tiempo);
+                if (partidaActual[$nickname].toFind[i] == " ") {
+                    partidaActual[$nickname].finding[i] = " ";
+                } else {
+                    partidaActual[$nickname].finding[i] = "-";
+                }
             }
-            localStorage.setItem("partida", JSON.stringify(partidaActual));
-        }, 1000);
-    }
-    /**/
+        }
 
+        $finding.text(partidaActual[$nickname].finding.join(""));
+        console.log(partidaActual);
+        /**/
 
-    /* PRUEBA LETRAS Y BUSCA RESULTADO */
-    for (var i = 0; i < $(".botones-abecedario > button").length; i++) {
-        $(".botones-abecedario > button").eq(i).on('click', function () {
-            letraClick(this);
+        /* PRUEBA LETRAS Y BUSCA RESULTADO */
+        for (var i = 0; i < $(".botones-abecedario > button").length; i++) {
+            $(".botones-abecedario > button").eq(i).on('click', function () {
+                letraClick(this);
+            });
+
+            for (var j = 0; j < partidaActual[$nickname].letrasUsadas.length; j++) {
+                if (($(".botones-abecedario > button").eq(i).text()) == partidaActual[$nickname].letrasUsadas[j]) {
+                    $(".botones-abecedario > button").eq(i).attr("disabled", true);
+                }
+            }
+        }
+        /**/
+
+        /* PISTA */
+        if (!partidaActual[$nickname].pista) {
+            $("#pista").attr("disabled", true);
+        }
+        $("#pista").on("click", function () {
+            partidaActual[$nickname].pista = false;
+            partidaActual[$nickname].tiempo -= 10;
+            this.disabled = true;
         });
-
-        for (var j = 0; j < partidaActual.letrasUsadas.length; j++) {
-            if (($(".botones-abecedario > button").eq(i).text()) == partidaActual.letrasUsadas[j]) {
-                $(".botones-abecedario > button").eq(i).attr("disabled", true);
-            }
-        }
+        /**/
     }
+
 
 
     function letraClick(evento) {
         var error = true;
         console.log(evento.innerHTML);
 
-        for (var i = 0; i < partidaActual.toFind.length; i++) {
-            if (partidaActual.toFind[i] == evento.innerHTML) {
+        for (var i = 0; i < partidaActual[$nickname].toFind.length; i++) {
+            if (partidaActual[$nickname].toFind[i] == evento.innerHTML) {
                 console.log("COINCIDE");
-                partidaActual.finding[i] = evento.innerHTML;
-                $finding.text(partidaActual.finding.join(""));
+                partidaActual[$nickname].finding[i] = evento.innerHTML;
+                $finding.text(partidaActual[$nickname].finding.join(""));
 
-                if ($finding[0].innerText == partidaActual.toFind) {
+                if ($finding[0].innerText == partidaActual[$nickname].toFind) {
                     sumarGanada();
                 }
                 error = false;
@@ -181,65 +162,64 @@ $(document).ready(function () {
         }
 
         if (error == true) {
-            if (partidaActual.numeroVidas > 1) {
-                --partidaActual.numeroVidas;
+            if (partidaActual[$nickname].numeroVidas > 1) {
+                --partidaActual[$nickname].numeroVidas;
             } else {
-                partidaActual.numeroVidas = 0;
+                partidaActual[$nickname].numeroVidas = 0;
                 sumarPerdida();
             }
-            $("#vidas").text(partidaActual.numeroVidas);
+            $("#vidas").text(partidaActual[$nickname].numeroVidas);
         }
         evento.disabled = true;
 
-        partidaActual.letrasUsadas.push(evento.innerHTML);
+        partidaActual[$nickname].letrasUsadas.push(evento.innerHTML);
         console.log(partidaActual);
     }
     /**/
 
+    /* TIEMPO RESTANTE */
+    function countDown() {
+        $tiempoRestante = $("#tiempo-restante");
+        $tiempoRestante.text(partidaActual[$nickname].tiempo);
 
-    /* PISTA */
-    if (!partidaActual.pista) {
-        $("#pista").attr("disabled", true);
-    }
-    $("#pista").on("click", function () {
-        pistaClick(this);
-    });
-
-    function pistaClick(evento) {
-        partidaActual.pista = false;
-        console.log(partidaActual);
-        console.log(evento.innerHTML);
-        clearInterval(timer);
-        countDown(($("#tiempo-restante").text()) - 10);
-        evento.disabled = true;
+        timer = setInterval(function () {
+            if (partidaActual[$nickname].tiempo == 0) {
+                sumarPerdida();
+                clearInterval(timer);
+            } else {
+                partidaActual[$nickname].tiempo = partidaActual[$nickname].tiempo - 1;
+                $tiempoRestante.text(partidaActual[$nickname].tiempo);
+            }
+            localStorage.setItem("partida", JSON.stringify(partidaActual));
+        }, 1000);
     }
     /**/
-    
-    /* REINICIAR */
-    $("#reiniciar").on("click", function () {
-        sumarPerdida();
-        location.reload();
-    });
 
     function sumarGanada() {
         $(".centrado").append($("<p>"), {
             text: "VICTORIA"
         });
         alert("Has ganado");
-        ++historico.ganadas;
-        $("#historico-ganadas").text(historico.ganadas);
+        ++partidaActual[$nickname].ganadas;
+        $("#historico-ganadas").text(partidaActual[$nickname].ganadas);
         clearInterval(timer);
-        localStorage.removeItem("partida");
-        localStorage.setItem("historico", JSON.stringify(historico));
+        localStorage.setItem("partida", JSON.stringify(partidaActual));
+        finPartida();
     }
 
     function sumarPerdida() {
         alert("Has perdido");
-        ++historico.perdidas;
-        $("historico-perdidas").text(historico.perdidas);
+        ++partidaActual[$nickname].perdidas;
+        $("historico-perdidas").text(partidaActual[$nickname].perdidas);
         clearInterval(timer);
-        localStorage.removeItem("partida");
-        localStorage.setItem("historico", JSON.stringify(historico));
+        localStorage.setItem("partida", JSON.stringify(partidaActual));
+        finPartida();
+    }
+
+    function finPartida() {
+        for (var i = 0; i < $(".botones-abecedario > button").length; i++) {
+            $(".botones-abecedario > button").eq(i).attr("disabled", true);
+        }
     }
 
 });
